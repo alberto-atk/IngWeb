41838d3d18700fb9dd4a7e8db291f9a8a0490528
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2022-01-25 23:55:46 +0100

    barra y foto

--- Frontend/locallibrary/catalog/views.py
+++ Frontend/locallibrary/catalog/views.py
@@ -5,6 +5,17 @@ def index(request):
     # Get an HttpRequest - the request parameter
     # perform operations using information from the request.
     # Return HttpResponse
-    context = {"1":"nav-link active"}
+    current_user = request.user
+    if current_user is not None:
+        context = {'Inicio':True,
+                    'Acercade':False,
+                    'Contacto':False,
+                    'Reservas':False,
+                    'user':current_user}
+    else:
+        context = {'Inicio':True,
+                    'Acercade':False,
+                    'Contacto':False,
+                    'Reservas':False}
     return render(request, 'index.html', context=context)
 # Create your views here.
